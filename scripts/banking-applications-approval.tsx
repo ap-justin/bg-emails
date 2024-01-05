c945f17c0aba98965b8e34ff44a7d596656697e8
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-01-05 13:58:40 +0800

    template request

--- scripts/banking-applications-approval.tsx
+++ scripts/banking-applications-approval.tsx
@@ -1,14 +1,13 @@
 import { render } from "jsx-email";
 import { Template, name } from "../src/templates/banking-applications-approval";
-import { Template as ITemplate } from "@aws-sdk/client-ses";
+import { CreateTemplateRequest } from "@aws-sdk/client-ses";
 
-const json: ITemplate = {
-  TemplateName: name,
-  SubjectPart: "Better giving: changes in banking details",
-  HtmlPart: await render(<Template />),
+const template: CreateTemplateRequest = {
+  Template: {
+    TemplateName: name,
+    SubjectPart: "Better giving: changes in banking details",
+    HtmlPart: await render(<Template />),
+  },
 };
 
-await Bun.write(
-  `./json-templates/${name}.json`,
-  JSON.stringify({ Template: json })
-);
+await Bun.write(`./json-templates/${name}.json`, JSON.stringify(template));
--- scripts/banking-applications-rejection.tsx
+++ scripts/banking-applications-rejection.tsx
@@ -3,12 +3,14 @@ import {
   Template,
   name,
 } from "../src/templates/banking-applications-rejection";
-import { Template as ITemplate } from "@aws-sdk/client-ses";
+import { CreateTemplateRequest } from "@aws-sdk/client-ses";
 
-const json: ITemplate = {
-  TemplateName: name,
-  SubjectPart: "Better giving: changes in banking details",
-  HtmlPart: await render(<Template />),
+const json: CreateTemplateRequest = {
+  Template: {
+    TemplateName: name,
+    SubjectPart: "Better giving: changes in banking details",
+    HtmlPart: await render(<Template />),
+  },
 };
 
 await Bun.write(
